c56df4f3a8f204a2cd19cf31ec8e52b3d960318f
Shiv <skagrawal10@gmail.com>
2016-07-06 11:17:58 +0530

    First Upload

---
+++ GoogleKeywordPlanner/Program.cs
@@ -0,0 +1,19 @@
+﻿using Google.Api.Ads.AdWords.Lib;
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace SK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //GetKeywordIdeas codeExample = new GetKeywordIdeas();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # GoogleKeywordPlanner
+
+GoogleKeywordPlanner is a .Net based application which consumes the Google Keyplanner API for generating keyword suggestions based on given keyword along with their statistics like monthly search volume. 
+
